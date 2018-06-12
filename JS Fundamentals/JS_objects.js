2a68d1a885fb810027fd47468dd0c490b62aec5e
Sarah Grayer <slawler689@gmail.com>
2018-06-12 17:44:05 -0500

    js objects

---
+++ JS Fundamentals/JS_objects.js
@@ -0,0 +1,61 @@
+//Challenge 1
+const students = [
+  {name: 'Remy', cohort: 'Jan'},
+  {name: 'Genevieve', cohort: 'March'},
+  {name: 'Chuck', cohort: 'Jan'},
+  {name: 'Osmund', cohort: 'June'},
+  {name: 'Nikki', cohort: 'June'},
+  {name: 'Boris', cohort: 'June'},
+];
+
+for (const student of students){
+  console.log(`Name: ${student.name}, Cohort: ${student.cohort}`)
+}
+
+//Challenge 2
+let users = {
+  employees: [
+    {'first_name': 'Miguel', 'last_name': 'Jones'},
+    {'first_name': 'Ernie', 'last_name': 'Bertson'},
+    {'first_name': 'Nora', 'last_name': 'Lu'},
+    {'first_name': 'Sally', 'last_name': 'Barkyoumb'},
+  ],
+  managers: [
+    {'first_name': 'Lillian', 'last_name': 'Chambers'},
+    {'first_name': 'Gordon', 'last_name': 'Poe'},
+  ]
+};
+for (const key in users){ //do below for each key in users, first 'employees', then 'managers'
+  console.log(key.toUpperCase());
+  for (let i = 0; i<users[key].length; i++){ //i is the instance, loop through each
+    const num = i+1; //const is a type of variable, stays constant
+    const first = users[key][i].first_name; //define variable 'first' wtih value of the key 'first_name' from instance i
+    const last = users[key][i].last_name;
+    const length = first.length + last.length;
+    console.log(`${num} - ${last}, ${first} - ${length}`);
+  }
+};
+
+
+
+//Basics from lecture 1
+var array = ['cat', 'dog', 'fish'];
+
+for(var index in array){
+  //console.log(index) //prints index (0, 1, 2)
+}
+
+for(var index of array){ //gives element of array
+  //console.log("this is ===>", index);
+}
+
+for (var thing of array.entries()){
+  //console.log(index); entries creates an array with each element. prints an array with the index, element
+  var index = thing[0];
+  var element = thing[1];
+  //console.log(index, element) //extracts and prints the index, element, no array
+}
+
+for (var[index, element] of array.entries()){
+  //console.log(index, element); //shorter way to print above. destructure content, extracts and immediately puts in variable names.
+}
