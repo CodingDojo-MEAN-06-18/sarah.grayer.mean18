e370fb1fb63fc93585a39c66c2c150f6d7344573
Sarah Grayer <slawler689@gmail.com>
2018-07-04 17:22:50 -0500

    quoting_dojo

---
+++ MongoDB/Mongoose/quoting_dojo/server.js
@@ -0,0 +1,62 @@
+
+const express=require("express");
+const bodyParser=require("body-parser");
+const mongoose=require("mongoose");
+const path=require("path");
+const port=8000;
+const app=express();
+const session=require("express-session")
+const flash=require("express-flash");
+app.use(session({
+  secret: 'super_secret',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 }
+}))
+
+app.use(flash());
+app.use(bodyParser.urlencoded({ extended:true })); //set up body-parser to parse form data
+
+mongoose.connect('mongodb://localhost/quoting_dojo')//set up db connection
+const quoteSchema = new mongoose.Schema({ //set up Schema
+  name: {type:String, required:true},
+  quote: {type:String, required:true}
+}, {timestamps: true });
+const Quote = mongoose.model('quotes', quoteSchema);//set up model, set quoteSchema in models as 'quotes'
+
+app.use(express.static(path.join(__dirname, "./static"))); //tells server where static files are stored (.css, referenced in .ejs files)
+
+app.set("views", path.join(__dirname, "./views")); //directs server to views folder for files to render
+app.set("view engine", "ejs");
+
+//routes below
+app.get('/', function(req, res){
+  res.render('welcome');
+});
+
+app.get('/quotes', function(req, res){ //gets all existing quotes and pass into the rendered quotes.ejs view
+  Quote.find({}, function(err, quotes){
+    if (err){
+      console.log(err);
+    }
+    res.render('quotes', {quotes:quotes});
+  });
+});
+
+app.post('/quotes', function(req, res){ //creates new quote and sends to '/quotes', rendering quotes.ejs view
+  Quote.create(req.body, function(err){
+    if (err){
+      console.log("We have an error");
+      for(var key in err.errors){
+        req.flash("required", err.errors[key].message); //messages is not interable, need them to be part of something to iterate
+      }
+      res.redirect('/');
+    }
+    else{
+      console.log("Successfully added a quote")
+      res.redirect('/quotes');
+    }
+  });
+});
+
+app.listen(port); //set above
