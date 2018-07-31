1b458d7a80ec42229c652a94c6df243cfe68c029
Sarah Grayer <slawler689@gmail.com>
2018-07-31 16:49:48 -0500

    move books

--- Angular/Component/Books/src/app/models/book.ts
+++
@@ -1,8 +0,0 @@
-export class Book { //create class here w/.ts file, import it into component.ts. Give the shape of the class info here
-    id: number;
-    title: string;
-    author: string;
-    pages: number;
-    year: number;
-    publisher: string;
-} 
--- Angular/Component/Books/src/app/services/index.ts
+++
@@ -1,4 +0,0 @@
-import {BookService} from './book.service';
-
-export const services: any[] = [BookService]; //now can access an array of books, comment on books, etc.
-export * from './book.service'; 
